4984db22c4a998dc900b7a26469a0a68f98bb6d9
Nicola Gallazzi <Nicola Gallazzi>
2017-02-22 11:01:02 +0100

    first commit

--- app/src/main/java/com/ngallazzi/enlightingviewexample/MainActivity.java
+++ app/src/main/java/com/ngallazzi/enlightingviewexample/MainActivity.java
@@ -1,10 +1,15 @@
 package com.ngallazzi.enlightingviewexample;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.ngallazzi.enlightinglayout.EnlightingLayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     private EnlightingLayout myEnlightingLayoutK,myEnlightingLayoutB,myEnlightingLayoutG,myEnlightingLayoutT;
@@ -31,5 +36,34 @@ public class MainActivity extends AppCompatActivity {
         myEnlightingLayoutB.setOnClickListener(clickListener);
         myEnlightingLayoutG.setOnClickListener(clickListener);
         myEnlightingLayoutT.setOnClickListener(clickListener);
+
+        TimerTask timerTask = new ShowProgressBarTask();
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(timerTask, 0, TimeUnit.SECONDS.toMillis(5));
+    }
+
+    class ShowProgressBarTask extends TimerTask {
+        Handler handler = new Handler();
+        @Override
+        public void run() {
+            try {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        myEnlightingLayoutT.setEnlighted(false);
+                        myEnlightingLayoutT.showProgressBar();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                myEnlightingLayoutT.setEnlighted(true);
+                                myEnlightingLayoutT.hideProgressBar();
+                            }
+                        },2500);
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
